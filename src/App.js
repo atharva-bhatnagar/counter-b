2592eed4358a811ed0e195212d01f1d38f871ede
atharva-bhatnagar <87374523+atharva-bhatnagar@users.noreply.github.com>
2023-01-30 19:45:34 +0530

    counter completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [counter,setCounter]=useState(0)
+
+
+  useEffect(()=>{
+    if(counter<0){
+      document.title=`Negative -> (${counter})`
+    }
+    if(counter>0){
+      document.title=`Positive -> (${counter})`
+    }
+    if(counter===0){
+      document.title="Counter App"
+    }
+  },[counter])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>A Counter Application Using<br/> useState()</h2>
+      <div className='counter-container'>
+        <h1>Count</h1>
+        <p id='count-text'>{counter}</p>
+        <div className='btn-box'>
+
+          <button id='inc' onClick={()=>{setCounter(counter+1)}}>Increment</button>
+          <button id='dec' onClick={()=>{setCounter(counter-1)}}>Decrement</button>
+        </div>
+      </div>
+      <p>Made By <em><strong>Rajnish Tripathi</strong></em> - 12011284</p>
     </div>
   );
 }
